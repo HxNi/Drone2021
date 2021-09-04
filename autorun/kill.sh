8b352445fa1fc047b86963376302b1077ab09a5c
HxNi <khkj9603@naver.com>
2021-09-04 14:18:01 +0900

    v.2

--- autorun/kill.sh
+++ autorun/kill.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-killall depth_noise.sh
-killall mavros.sh
-killall px4.sh
-killall airsim.sh
+if [ $# -eq 1 ]
+then
+	if [ $1 = "ue" ]
+	then killall UE4Editor; killall UE4Editor
+	else
+	echo "OPTION: ue\r"
+	fi
+else
+	killall depth_noise.sh
+	killall mavros.sh
+	killall px4.sh
+	killall airsim.sh
+fi
